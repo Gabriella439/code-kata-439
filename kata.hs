61915b4fd8b789967c80c350eaabe0bb8fef4796
Gabriel Gonzalez <Gabriel439@gmail.com>
2013-05-18 15:19:38 -0700

    Added Frames

--- kata.hs
+++ kata.hs
@@ -1,13 +1,21 @@
 type Roll  = Int
 type Score = Int
 
-score :: [Roll] -> Score
+data Frame = Strike | Pair Roll Roll
+
+rollsToFrames :: [Roll] -> Maybe [Frame]
+rollsToFrames rolls = case rolls of
+    []         -> Just []
+    10:rolls'  -> fmap (Strike:)   (rollsToFrames rolls') 
+    x:[]       -> Nothing
+    x:y:rolls' -> fmap (Pair x y:) (rollsToFrames rolls')
+
+score :: [Frame] -> Score
 score = go 1 1
   where
     go m1 m2 rolls = case rolls of
-        []         -> 0
-        10:rolls'  -> 10 * m1 + go (m2 + 1) 2 rolls'
-        x:[]       -> x  * m1
-        x:y:rolls' ->
+        []              -> 0
+        Strike:rolls'   -> 10 * m1 + go (m2 + 1) 2 rolls'
+        Pair x y:rolls' ->
             x * m1 + y * m2
           + (if (x + y) == 10 then go 2 else go 1) 1 rolls'
